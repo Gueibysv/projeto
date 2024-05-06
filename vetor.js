409cf44a83e9e809d1b09bed94d7e12da26511bb
Gueibysv <Gueibysv1@gmail.com>
2024-05-06 08:47:16 +0100

    VetorVersão2

--- vetor.js
+++ vetor.js
@@ -1,7 +1,12 @@
+var vetor = [5,6,7,8,9];
+
 function somarVetor(vetor) {
     let soma = 0;
     for (let i = 0; i < vetor.length; i++) {
         soma += vetor[i];
     }
+    console.log(soma);
     return soma;
 }
+
+somarVetor(vetor);
